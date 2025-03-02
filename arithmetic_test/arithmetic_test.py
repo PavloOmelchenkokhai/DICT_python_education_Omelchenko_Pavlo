9ad101716e2ef305efc63dea3cc3d7f5e6a6f9e8
PavloOmelchenkoKhai <p.v.omelchenko@student.khai.edu>
2025-03-02 15:32:30 +0200

    arithmetic_test{2}Stage

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,37 +1,41 @@
 import random
 
 
-def generate_task():
+def generate_question():
+    """Генерує просте арифметичне завдання."""
     num1 = random.randint(2, 9)
     num2 = random.randint(2, 9)
-    operator = random.choice(['+', '-', '*'])
+    operation = random.choice(['+', '-', '*'])
+    question = f"{num1} {operation} {num2}"
+    correct_answer = eval(question)
+    return question, correct_answer
 
-    print(f"{num1} {operator} {num2}")
-    return num1, num2, operator
 
-
-def get_correct_answer(num1, num2, operator):
-    if operator == '+':
-        return num1 + num2
-    elif operator == '-':
-        return num1 - num2
-    elif operator == '*':
-        return num1 * num2
+def get_valid_input():
+    """Отримує коректне числове введення користувача."""
+    while True:
+        user_input = input(">")
+        if user_input.lstrip('-').isdigit():
+            return int(user_input)
+        print("Incorrect format.")
 
 
 def main():
-    num1, num2, operator = generate_task()
+    score = 0
+    total_questions = 5
 
-    try:
-        user_answer = int(input())
-        correct_answer = get_correct_answer(num1, num2, operator)
+    for _ in range(total_questions):
+        question, correct_answer = generate_question()
+        print(question)
+        user_answer = get_valid_input()
 
         if user_answer == correct_answer:
             print("Right!")
+            score += 1
         else:
             print("Wrong!")
-    except ValueError:
-        print("Invalid input. Please enter a number.")
+
+    print(f"Your mark is {score}/{total_questions}.")
 
 
 if __name__ == "__main__":
