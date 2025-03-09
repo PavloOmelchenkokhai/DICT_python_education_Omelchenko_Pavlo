fcbd0d5cbddc2661f87170aafe9c57b0c8fd5bcc
PavloOmelchenkoKhai <p.v.omelchenko@student.khai.edu>
2025-03-09 15:29:22 +0200

    rock_paper_scissors{3}Stage

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -8,15 +8,21 @@ winning_moves = {
 
 options = list(winning_moves.keys())
 
-user_choice = input().strip().lower()
-computer_choice = random.choice(options)
+while True:
+    user_choice = input().strip().lower()
 
-if user_choice in winning_moves:
-    if user_choice == computer_choice:
-        print(f"There is a draw ({computer_choice})")
-    elif winning_moves[user_choice] == computer_choice:
-        print(f"Well done. The computer chose {computer_choice} and failed")
+    if user_choice == "!exit":
+        print("Bye!")
+        break
+
+    if user_choice in winning_moves:
+        computer_choice = random.choice(options)
+
+        if user_choice == computer_choice:
+            print(f"There is a draw ({computer_choice})")
+        elif winning_moves[user_choice] == computer_choice:
+            print(f"Well done. The computer chose {computer_choice} and failed")
+        else:
+            print(f"Sorry, but the computer chose {computer_choice}")
     else:
-        print(f"Sorry, but the computer chose {computer_choice}")
-else:
-    print("Invalid input. Please choose rock, paper, or scissors.")
+        print("Invalid input.")
