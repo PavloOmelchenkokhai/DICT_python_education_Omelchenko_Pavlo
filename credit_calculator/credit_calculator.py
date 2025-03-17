f8bd2c8f737d7918fafe027cd9c0498fe3cbe758
PavloOmelchenkoKhai <p.v.omelchenko@student.khai.edu>
2025-03-17 20:44:00 +0200

    credit_calculator{2}Stage

--- credit_calculator/credit_calculator.py
+++ credit_calculator/credit_calculator.py
@@ -1,5 +1,35 @@
-print("Loan principal: 1000")
-print("Month 1: repaid 250")
-print("Month 2: repaid 250")
-print("Month 3: repaid 500")
-print("The loan has been repaid!")
+import math
+
+# Запитуємо суму кредиту
+principal = int(input("Enter the loan principal:\n> "))
+
+# Запитуємо, що користувач хоче обчислити
+print("What do you want to calculate?")
+print('type "m" – for number of monthly payments,')
+print('type "p" – for the monthly payment:')
+choice = input("> ").strip()
+
+if choice == "m":
+    # Якщо користувач хоче обчислити кількість місяців
+    monthly_payment = int(input("Enter the monthly payment:\n> "))
+
+    months = math.ceil(principal / monthly_payment)
+
+    if months == 1:
+        print("It will take 1 month to repay the loan")
+    else:
+        print(f"It will take {months} months to repay the loan")
+
+elif choice == "p":
+    # Якщо користувач хоче обчислити щомісячний платіж
+    months = int(input("Enter the number of months:\n> "))
+
+    payment = math.ceil(principal / months)
+    last_payment = principal - (months - 1) * payment
+
+    if payment == last_payment:
+        print(f"Your monthly payment = {payment}")
+    else:
+        print(f"Your monthly payment = {payment} and the last payment = {last_payment}.")
+else:
+    print("Invalid choice")
